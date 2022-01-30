bdcf2114ce9599c88b8a450cc633850d6ce48b46
Ravi Singh <ravi@appsimpact.com>
2022-01-30 23:06:18 +0530

    signin in with email or username + custom attrs

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,30 @@ class User < ApplicationRecord
          :rememberable, 
          :validatable, 
          :confirmable, 
-         :trackable
+         :trackable,
+         authentication_keys: [:login]
+
+  validates :username, presence: true, uniqueness: true
+
+  attr_accessor :login
+
+  def login
+    @login || self.username || self.email
+  end
 
   private
 
   def after_confirmation
     WelcomeMailer.send_greetings_notification(self)
                  .deliver_now
+  end
+
+  def self.find_for_database_authentication(warden_condition)
+    conditions = warden_condition.dup
+    if(login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_174612) do
+ActiveRecord::Schema.define(version: 2022_01_30_155306) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,6 +29,11 @@ ActiveRecord::Schema.define(version: 2022_01_29_174612) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "contact_number"
+    t.string "address"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
